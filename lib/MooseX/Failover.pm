01056f8a2bff34ce5fdad4fe18584915e2f278b0
Robert Rothenberg <rrwo@thermeon.com>
2014-12-08 18:04:00 +0000

    Bumped to v0.1.3

--- lib/MooseX/Failover.pm
+++ lib/MooseX/Failover.pm
@@ -6,7 +6,7 @@ use Carp;
 use Class::Load qw/ try_load_class /;
 use PerlX::Maybe;
 
-use version 0.77; our $VERSION = version->declare('v0.1.2');
+use version 0.77; our $VERSION = version->declare('v0.1.3');
 
 # RECOMMEND PREREQ: Class::Load::XS
 # RECOMMEND PREREQ: PerlX::Maybe::XS
